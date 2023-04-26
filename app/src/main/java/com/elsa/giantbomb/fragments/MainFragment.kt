d02380ea06fb4818edaa548f55ca498f29f5efc1
Count-Monte <edmund.mcgardner@gmail.com>
2023-04-26 15:51:07 -0400

    initial kotlin with error

---
+++ app/src/main/java/com/elsa/giantbomb/fragments/MainFragment.kt
@@ -0,0 +1,11 @@
+package com.elsa.giantbomb.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
